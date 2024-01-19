702fbbf769a3e34f0ea3f60509ad6d4f53e3f799
Susep Sp <susep.dev@gmail.com>
2024-01-19 15:33:05 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 
 error_reporting(0);
 
-echo '<a href="?page=home">Home</a>';
+echo '<a href="?page=home">Home</a> | ';
 echo '<a href="?page=about">About</a>';
 echo '<br>';
 echo $_GET['page']?:'home';
